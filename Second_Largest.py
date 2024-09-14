be59d3cbee59c41c4c1e476ec5617cce5aafa5ce
ShreyasBhagat2802 <147151828+ShreyasBhagat2802@users.noreply.github.com>
2024-09-14 12:16:45 +0530

    Update Second_Largest.py

--- Second_Largest.py
+++ Second_Largest.py
@@ -1,27 +1,27 @@
 #Program to find second largest element
 number_list = []
-number_count = int(input("Enter number of item in the list"))
+number_count = int(input("Enter number of item in the list "))
 
 for i in range(1, (number_count+1)):
     number = int(input('Enter the number '))
     number_list.append(number)
 print(number_list)
 
-x = 0
-y = 0
+largest = 0
+second_largest = 0
 
 for i in number_list:
     if len(number_list) == 1:
         print("Only one number")
 
     elif len(number_list) > 1:
-        if x == i:
+        if largest == i:
             pass
-        elif x < i:
-            y = x
-            x = i
-        elif x > i:
+        elif largest < i:
+            second_largest = largest
+            largest = i
+        elif largest > i:
             pass
-print(y)
+print(second_largest)
 
     
